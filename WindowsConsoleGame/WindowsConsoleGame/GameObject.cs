a3481cb8a7b2b1ed51813f74c1c72bd9693eadf4
Gravn <jesper.greve@gmail.com>
2015-03-09 13:41:57 +0100

    Cloning

---
+++ WindowsConsoleGame/WindowsConsoleGame/GameObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsConsoleGame
+{
+    class GameObject
+    {
+
+        private Vector2 position;
+
+        public Vector2 pubposition
+        {
+            get {return position; }
+            set { position = value; }
+        }
+        private ConsoleColor color;
+        private Draw d;
+
+        public GameObject(Vector2 position,ConsoleColor color)
+        {
+            this.d = Program.d;
+            this.position = position;
+            this.color = color;
+            
+        }
+
+        public void Draw()
+        {
+            Console.BackgroundColor = color;
+            d.Point(position);
+        }
+
+        public GameObject Clone()
+        {
+            GameObject g = (GameObject)this.MemberwiseClone();
+            g.pubposition = new Vector2(16, 16);
+            return g;
+        }
+
+
+
+    }
+}
--- WindowsConsoleGame/WindowsConsoleGame/Program.cs
+++ WindowsConsoleGame/WindowsConsoleGame/Program.cs
@@ -8,54 +8,38 @@ namespace WindowsConsoleGame
 {
     class Program
     {
+
+        public static Draw d = new Draw();
+        
+
         static void Main(string[] args)
         {
             Console.SetWindowSize(64, 64);
             Console.SetBufferSize(64, 64);
             Console.CursorSize = 100;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Clear();
 
+            GameObject[] objects = new GameObject[10];
 
+            objects[0] = new GameObject(new Vector2(10, 10), ConsoleColor.Green);
 
-            Draw d = new Draw();
-            Vector2 a = new Vector2(16, 16);
-            Vector2 b = new Vector2(48, 48);
-            double anglea =0;
-            double angleb = 3.14f;
-
-            while (true)
-            { 
-                if(anglea>Math.PI)
-                {
-                    anglea-=2*Math.PI;
-                }
-
-                anglea+=0.1f;
-
-                if (angleb >Math.PI)
-                {
-                    angleb -=2*Math.PI;
-                }
-
-                angleb += 0.1f;
-
-                Console.BackgroundColor = ConsoleColor.Black;
-                d.Line(a, b);
-
-
-                a.x = Math.Sin(anglea) * 16 + 32;
-                a.y = Math.Cos(anglea) * 16 + 32;
-
-                b.x = Math.Sin(angleb) * 16 + 32;
-                b.y = Math.Cos(angleb) * 16 + 32;
-
-                Console.BackgroundColor = ConsoleColor.White;
-                d.Line(a, b);
-
-                System.Threading.Thread.Sleep(10);
+            for (int i = 1; i < 10; i++)
+            {
+                objects[i] = objects[0].Clone();
             }
+
+            Random r = new Random();
+
+            objects[5].pubposition.x = 5;
+
+            foreach (GameObject o in objects)
+            {
+                o.Draw();
+            }
+
+            System.Threading.Thread.Sleep(10);
             
-
-
             Console.ReadKey();
         }
     }
